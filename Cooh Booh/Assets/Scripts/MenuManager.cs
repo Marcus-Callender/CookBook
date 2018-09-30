47fdf0c3c6cd4df7ba954f5acef42ceafd83b4d8
Marcus Callender <m.shute@outlook.com>
2018-09-30 15:47:08 +0100

    Initial project commit

---
+++ Cooh Booh/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoohBooh
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] m_menuPrefabs;
+
+        [SerializeField]
+        private Transform m_menuParent;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
