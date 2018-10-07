4290a5c1269fa6f1b17f2982e5145cdbe3fb20d5
Marcus Callender <m.shute@outlook.com>
2018-10-07 15:20:59 +0100

    Code and objects for  dropdown menus

---
+++ Cooh Booh/Assets/Scripts/DropDownMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropDownMenu : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private GameObject m_expandedMenu;
+
+    [SerializeField]
+    private GameObject m_expandIndicator;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + " Was Clicked.");
+        m_expandedMenu.SetActive(!m_expandedMenu.activeSelf);
+        m_expandIndicator.transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f), 180.0f);
+    }
+}
