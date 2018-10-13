35b0c224427cee597a0d66ab5f34b1f59fc7c626
Marcus Callender <m.shute@outlook.com>
2018-10-13 14:14:28 +0100

    Created add recepie button

---
+++ Cooh Booh/Assets/Scripts/AddNewRecepie.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AddNewRecepie : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private GameObject m_recepiePrefab;
+
+    [SerializeField]
+    private GameObject m_locationToAdd;
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + " Was Clicked.");
+
+        GameObject newRecepie = Instantiate(m_recepiePrefab, m_locationToAdd.transform);
+
+        newRecepie.transform.SetSiblingIndex(newRecepie.transform.GetSiblingIndex() - 1);
+    }
+}
