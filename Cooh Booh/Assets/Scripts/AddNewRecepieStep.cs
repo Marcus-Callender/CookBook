0c5ca801bd2199dff798d8d59a37d2919310e758
Marcus Callender <m.shute@outlook.com>
2018-10-13 21:26:57 +0100

    Add recepie step screen

---
+++ Cooh Booh/Assets/Scripts/AddNewRecepieStep.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class AddNewRecepieStep : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private GameObject m_recepiePrefab;
+
+    [SerializeField]
+    private GameObject m_locationToAdd;
+
+    [SerializeField]
+    private LayoutGroup m_layoutGroup;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + " Was Clicked.");
+
+        GameObject newRecepie = Instantiate(m_recepiePrefab, m_locationToAdd.transform);
+
+        Canvas.ForceUpdateCanvases();
+        m_layoutGroup.enabled = false;
+        m_layoutGroup.enabled = true;
+    }
+}
