10997891fb037cddc3f4243705afca25b3c22bc6
Marcus Callender <m.shute@outlook.com>
2019-01-05 16:35:12 +0000

    Minor script changes

--- Cooh Booh/Assets/Scripts/AddNewRecepieStep.cs
+++ Cooh Booh/Assets/Scripts/AddNewRecepieStep.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class AddNewRecepieStep : MonoBehaviour, IPointerDownHandler
+namespace CoohBooh
 {
-    [SerializeField]
-    private GameObject m_recepiePrefab;
-
-    [SerializeField]
-    private GameObject m_locationToAdd;
-
-    [SerializeField]
-    private LayoutGroup m_layoutGroup;
-
-    public void OnPointerDown(PointerEventData eventData)
+    public class AddNewRecepieStep : MonoBehaviour, IPointerDownHandler
     {
-        Debug.Log(gameObject.name + " Was Clicked.");
+        [SerializeField]
+        private GameObject m_recepiePrefab;
 
-        GameObject newRecepie = Instantiate(m_recepiePrefab, m_locationToAdd.transform);
+        [SerializeField]
+        private GameObject m_locationToAdd;
 
-        Canvas.ForceUpdateCanvases();
-        m_layoutGroup.enabled = false;
-        m_layoutGroup.enabled = true;
+        [SerializeField]
+        private LayoutGroup m_layoutGroup;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Debug.Log(gameObject.name + " Was Clicked.");
+
+            GameObject newRecepie = Instantiate(m_recepiePrefab, m_locationToAdd.transform);
+
+            Canvas.ForceUpdateCanvases();
+            m_layoutGroup.enabled = false;
+            m_layoutGroup.enabled = true;
+        }
     }
 }
---
+++ Cooh Booh/Assets/Scripts/SaveNewRecepie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace CoohBooh
+{
+    public class SaveNewRecepie : MonoBehaviour, IPointerDownHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+
+        }
+    }
+}
