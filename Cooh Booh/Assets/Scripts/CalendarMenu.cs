ec6dadcbc414c3a32c29ee9736ac1a8d3cc76ee6
Marcus Callender <m.shute@outlook.com>
2019-05-29 15:48:29 +0100

    Added cycling through months in calender tab.

--- Cooh Booh/Assets/Scripts/CalendarMenu.cs
+++ Cooh Booh/Assets/Scripts/CalendarMenu.cs
@@ -1,21 +1,95 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CoohBooh
 {
     public class CalendarMenu : MenuBase
     {
+        [SerializeField]
+        private Text monthYearText;
+
+        [SerializeField]
+        private DateTile[] tiles;
+
+        private DateTime DisplayDate;
+
+        private System.Globalization.DateTimeFormatInfo dtfi;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            dtfi = new System.Globalization.DateTimeFormatInfo();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        public override void OnBecomeActive()
+        {
+            //localDate = DateTime.Now;
+            //DateTime firstDayOfMonth = new DateTime(localDate.Year, localDate.Month, 1);
+            //int daysInMonth = System.DateTime.DaysInMonth(localDate.Year, localDate.Month);
+            
+            //Debug.Log("Date is: " + localDate.Day + "/" + localDate.Month + "/" + localDate.Year);
+            //Debug.Log("Time is: " + localDate.Hour + ":" + localDate.Minute + ":" + localDate.Second);
+            //Debug.Log("Day of week: " + localDate.DayOfWeek);
+
+            DisplayDate = DateTime.Now;
+            UpdateDate();
+        }
+
+        private void UpdateDate()
+        {
+            int daysInMonth = DateTime.DaysInMonth(DisplayDate.Year, DisplayDate.Month);
+            DayOfWeek dayOfWeek = new DateTime(DisplayDate.Year, DisplayDate.Month, 1).DayOfWeek;
+            
+            monthYearText.text = dtfi.GetMonthName(DisplayDate.Month) + " " + DisplayDate.Year;
+
+            for (int z = 0; z < tiles.Length; z++)
+            {
+
+                if (z < (int)dayOfWeek)
+                {
+                    tiles[z].SetDate(-1);
+                    tiles[z].SetInactive();
+                }
+                else if (z < (int)dayOfWeek + daysInMonth)
+                {
+                    tiles[z].SetDate((z - (int)dayOfWeek) + 1);
+                    tiles[z].SetActive();
+                }
+                else if (z >= (int)dayOfWeek + daysInMonth)
+                {
+                    tiles[z].SetDate(-1);
+                    tiles[z].SetInactive();
+                }
+
+
+                //tiles[z].SetInactive();
+            }
+
+            /*for (int z = (int)dayOfWeek; z < (int)dayOfWeek + daysInMonth; z++)
+            {
+                tiles[z].SetActive();
+            }*/
+        }
+
+        public void NextMonth()
+        {
+            DisplayDate = DisplayDate.AddMonths(1);
+            UpdateDate();
+        }
+
+        public void PreviousMonth()
+        {
+            DisplayDate = DisplayDate.AddMonths(-1);
+            UpdateDate();
         }
     }
 }
---
+++ Cooh Booh/Assets/Scripts/DateTile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CoohBooh
+{
+    public class DateTile : MonoBehaviour
+    {
+        public Image m_image;
+        public Button m_button;
+        public Text m_date;
+        public Text m_meals;
+
+        private void Awake()
+        {
+            m_button = GetComponent<Button>();
+
+            m_date = transform.GetChild(0).GetComponent<Text>();
+            m_meals = transform.GetChild(1).GetComponent<Text>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetDate(int date)
+        {
+            m_date.text = date.ToString();
+        }
+
+        public void SetActive()
+        {
+            m_button.interactable = true;
+        }
+
+        public void SetInactive()
+        {
+            m_button.interactable = false;
+        }
+    }
+}
